2383a4866f0eca31d974e3382542ab6c7479c574
SkillVirus <aeugo@naver.com>
2021-04-24 11:49:37 +0000

    printf 매개변수 순서 실습

--- src/002.Variable/Variable02.c
+++ src/002.Variable/Variable02.c
@@ -4,9 +4,14 @@ int main()
 {
 	int a = 127;
 	
-	printf("%d의 %d 진수 표현은 : %o \n", a, 8, a);
-	printf("%d의 %d 진수 표현은 : %d \n", a, 10, a);
-	printf("%d의 %d 진수 표현은 : %x \n", a, 16, a);
+	// printf("%d의 %d 진수 표현은 : %o \n", a, 8, a);
+	// printf("%d의 %d 진수 표현은 : %d \n", a, 10, a);
+	// printf("%d의 %d 진수 표현은 : %x \n", a, 16, a);
+    
+    //127의 177은 8진수 표현입니다.
+    printf("%d의 %o은 %d진수 표현입니다. \n", a, a, 8);
+    printf("%d의 %d은 %d진수 표현입니다. \n", a, a, 10);
+    printf("%d의 %x은 %d진수 표현입니다. \n", a, a, 16);
 	
 	return 0;
 }
