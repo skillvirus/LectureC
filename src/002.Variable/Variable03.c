addb9f9e990872d1f0acb9c726c106c09b189144
SkillVirus <aeugo@naver.com>
2021-04-25 05:43:56 +0000

    실수형 자료 예제

---
+++ src/002.Variable/Variable03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    float a = 3.141592f;
+    double b = 3.141592;
+    
+    printf("float a 는 %f 입니다. \n", a);
+    printf("double b 는 %f 입니다. \n", b);
+    
+    //return 0;
+}
